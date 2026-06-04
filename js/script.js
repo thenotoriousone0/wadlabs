b6bdea20cadd67fc2a8acea6d28648e1dae4b771
thenotoriousone0 <njau.mark@strathmore.edu>
2026-06-04 11:52:52 +0300

    lab 2.7 js 101

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let age = 20;
+var course = "BBIT";
+const university = "Strathmore";
+
+
+//Redeclare
+//let age=40;
+var course = "BBA";
+//const university = "Nairobi University";
+
+//Reassign
+age = 40;
+course = "BBA";
+//university = "Nairobi University";
+if (true) {
+    let time = "11:30";
+    var day = "4th";
+    const year = "2026";
+}
+console.log(time); 
+console.log(day);
+console.log(year);
+
